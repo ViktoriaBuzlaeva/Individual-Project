21785611a176e3db03a9d017d02ad333f1aeca55
ViktoriaBuzlaeva <vicktoria.buzlaeva@yandex.ru>
2025-05-16 19:25:10 +0300

    fixed code

--- GameStore/TestApplication/main.cpp
+++ GameStore/TestApplication/main.cpp
@@ -1002,6 +1002,73 @@ bool test_67_insert_after_delete() {
     return TestSystem::check(expected_result, actual_result);
 }
 
+bool test_68_replace_after_deleted() {
+    bool expected_result = true;
+    bool actual_result = true;
+    int* mass = new int[100];
+    for (int i = 0; i < 100; i++) {
+        mass[i] = i + 1;
+    }
+    TVector<int> v1(100, mass);
+    v1.erase(3);
+    v1.pop_front();
+    v1.pop_back();
+    v1.erase(3);
+    v1.replace(4, 55);
+
+    TVector<int> v2({ 2, 3, 5, 7, 55, 9, 10, 11, 12, 13, 14, 15, 16,
+        17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+        33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+        49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
+        65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+        81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96,
+        97, 98, 99 });
+    
+    actual_result &= (v1 == v2);
+
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test_69_insert_empty_tvector() {
+    bool expected_result = true;
+    bool actual_result = true;
+    TVector<int> v1;
+    v1.push_front(1);
+    TVector<int> v2({ 1 });
+    actual_result &= (v1 == v2);
+
+    TVector<int> v3;
+    v3.push_back(10);
+    TVector<int> v4({ 10 });
+    actual_result &= (v3 == v4);
+
+    TVector<int> v5;
+    v5.insert(0, 88);
+    TVector<int> v6({ 88 });
+    actual_result &= (v5 == v6);
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test_70_replace_by_find_pointer() {
+    bool expected_result = true;
+    bool actual_result = true;
+    TVector<int> v1({ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+        17, 18, 19, 20, 7, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
+        34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
+        52, 53, 54, 7, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68,
+        69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86,
+        7, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100 });
+    v1.replace(find_first_pointer(v1, 9), 99);
+    TVector<int> v2({ 1, 2, 3, 4, 5, 6, 7, 8, 99, 10, 11, 12, 13, 14, 15, 16,
+        17, 18, 19, 20, 7, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
+        34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
+        52, 53, 54, 7, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68,
+        69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86,
+        7, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100 });
+    actual_result &= (v1 == v2);
+    return TestSystem::check(expected_result, actual_result);
+}
+
 int main() {
     Application application_1;
     Date date_1;
@@ -1136,6 +1203,12 @@ int main() {
     TestSystem::start_test(test_66_sort_tvector, " sort_tvector");
     TestSystem::start_test(test_67_insert_after_delete,
         " insert_after_delete");
+    TestSystem::start_test(test_68_replace_after_deleted,
+        " replace_after_deleted");
+    TestSystem::start_test(test_69_insert_empty_tvector,
+        " insert_empty_tvector");
+    TestSystem::start_test(test_70_replace_by_find_pointer,
+        " replace_by_find_pointer");
 
     TestSystem::print_final_info();
 
