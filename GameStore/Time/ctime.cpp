5b46ee65b453ba16a57bdb54b30255efd0104dae
ViktoriaBuzlaeva <vicktoria.buzlaeva@yandex.ru>
2025-05-19 21:00:38 +0300

    added class declaration

--- GameStore/Time/ctime.cpp
+++ GameStore/Time/ctime.cpp
@@ -1,3 +1,7 @@
 // Copyright 2025 Viktoria Buzlaeva
 
 #include "/GitHub/Individual-Project/GameStore/Time/ctime.h"
+
+CTime::CTime(int hours, int minutes, int seconds) : _hours(hours),
+    _minutes(minutes), _seconds(seconds) {
+}
--- GameStore/Time/ctime.h
+++ GameStore/Time/ctime.h
@@ -1,5 +1,7 @@
 // Copyright 2025 Viktoria Buzlaeva
 
+#include <string>
+
 #ifndef GAMESTORE_TIME_CTIME_H_
 #define GAMESTORE_TIME_CTIME_H_
 
@@ -7,6 +9,17 @@ class CTime {
     int _hours;
     int _minutes;
     int _seconds;
+    
+ public:
+    CTime(int hours = 0, int minutes = 0, int seconds = 0);
+    CTime(const CTime&);
+    CTime(std::string);
+
+    inline const int hours() const noexcept;
+    inline const int minutes() const noexcept;
+    inline const int seconds() const noexcept;
+
+    void print() noexcept;
 };
 
 #endif  // GAMESTORE_TIME_CTIME_H_
