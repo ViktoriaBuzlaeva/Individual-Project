206784110497a502298226496894f77f929e21b6
ViktoriaBuzlaeva <vicktoria.buzlaeva@yandex.ru>
2025-05-29 19:28:56 +0300

    added class declaration

--- GameStore/Date/date.cpp
+++ GameStore/Date/date.cpp
@@ -1,3 +1,9 @@
 // Copyright 2025 Viktoria Buzlaeva
 
 #include "/GitHub/Individual-Project/GameStore/Date/date.h"
+
+Date::Date(int year, int month, int day) {
+	_year = year;
+	_month = month;
+	_day = day;
+}
--- GameStore/Date/date.h
+++ GameStore/Date/date.h
@@ -3,10 +3,30 @@
 #ifndef GAMESTORE_DATE_DATE_H_
 #define GAMESTORE_DATE_DATE_H_
 
+#include <iostream>
+
 class Date {
     int _year;
     int _month;
     int _day;
+
+public:
+    Date(int year = 1, int month = 1, int day = 2000);
+    Date(const Date&);
+
+    const int& year() const noexcept;
+    const int& month() const noexcept;
+    const int& day() const noexcept;
+
+    bool operator < (const Date&)const noexcept;
+    bool operator == (const Date&) const noexcept;
+
+    std::string to_string() const noexcept;
+
+private:
+    bool is_correct(int, int, int);
+    bool is_leap_year(int);
+    int days_in_month();
 };
 
 #endif  // GAMESTORE_DATE_DATE_H_
