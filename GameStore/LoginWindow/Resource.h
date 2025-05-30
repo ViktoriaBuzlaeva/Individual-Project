67bb6b0ad4f6a99dfe6de0c07b29d2b85c355989
ViktoriaBuzlaeva <vicktoria.buzlaeva@yandex.ru>
2025-05-30 21:20:00 +0300

    fixed code

--- GameStore/LoginWindow/Resource.h
+++ GameStore/LoginWindow/Resource.h
@@ -2,3 +2,8 @@
 // {{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by app.rc
+
+#ifndef GAMESTORE_LOGINWINDOW_RESOURCE_H_
+#define GAMESTORE_LOGINWINDOW_RESOURCE_H_
+
+#endif  // GAMESTORE_LOGINWINDOW_RESOURCE_H_
